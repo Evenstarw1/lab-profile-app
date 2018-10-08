0393b7c9f97396dd9de14050774e8a65f8aca528
Evenstarw1 <lauracorrallopez@gmail.com>
2018-10-09 01:42:45 +0200

    Tercera a medias

--- lab-profile-app/front/src/App.js
+++ lab-profile-app/front/src/App.js
@@ -1,25 +1,27 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
+import Signup from './components/auth/Signup';
 
 class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = { loggedInUser: null };
+  }
+
+  getTheUser= (userObj) => {
+    this.setState({
+      loggedInUser: userObj
+    })
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div >
+        <Switch>
+          <Route exact path="/signup" component={Signup} />
+        </Switch>
       </div>
     );
   }
